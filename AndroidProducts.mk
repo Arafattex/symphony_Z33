8baaf1af388e09d8cec883cdbe7cf09a9a613628
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-06 12:06:57 +0200

    Z33: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: symphony Model: Z33 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Z33.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Z33-user \
+    omni_Z33-userdebug \
+    omni_Z33-eng
